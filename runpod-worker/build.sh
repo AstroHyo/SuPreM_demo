65f4fa8df5c121cd5d6f11218e2f232e2cce7003
AstroHyo <spinelee2002@gmail.com>
2024-10-03 06:41:33 -0400

    Edit build.sh

--- runpod-worker/build.sh
+++ runpod-worker/build.sh
@@ -1,4 +1,4 @@
 TAG=$(git rev-parse --short HEAD)
 
-docker build --tag astrohyo/suprem-runpod-worker:${TAG} .
+docker build --platform linux/amd64 --tag astrohyo/suprem-runpod-worker:${TAG} .
 docker push astrohyo/suprem-runpod-worker:${TAG}
